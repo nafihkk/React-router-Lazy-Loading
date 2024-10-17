42d5c4c6bbb7c685c016c15a11fe1fada689ba80
nafihkk <muhammadnafihkk786@gmail.com>
2024-10-17 09:13:40 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css"
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+<BrowserRouter>
+<App/>
+</BrowserRouter>  
+  </React.StrictMode>
+);
